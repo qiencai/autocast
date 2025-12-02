81c50bf5805930af9489af8f0d6983e7fc8a5105
marjanfamili <marjanfamili@gmail.com>
2025-12-02 13:57:47 +0000

    added noise scheduler and a notebook to make sure the input runs through the model

--- src/auto_cast/processors/diffusion.py
+++ src/auto_cast/processors/diffusion.py
@@ -1,16 +1,42 @@
+import math
+
 import torch
 import torch.nn as nn
-from auto_cast.types import Batch, EncodedBatch, RolloutOutput, Tensor
+
 from auto_cast.processors.base import Processor
+from auto_cast.types import Batch, EncodedBatch, RolloutOutput, Tensor
 
-from azula.noise import Schedule
 
+class NoiseSchedule(nn.Module):
+    """Noise Schedule Module."""
+
+    def forward(self, t: Tensor) -> tuple[Tensor, Tensor]:
+        """Get alpha and sigma for given time steps t."""
+        msg = "Subclasses should implement this method."
+        raise NotImplementedError(msg)
+    
+
+class LogLinearSchedule(NoiseSchedule):
+    """Log-Linear Noise Schedule.
+    
+    Implements a log-linear schedule for alpha and sigma.
+    """
+
+    def __init__(self, sigma_min: float = 0.002, sigma_max: float = 80.0):
+        super().__init__()
+        self.log_sigma_min = math.log(sigma_min)
+        self.log_sigma_max = math.log(sigma_max)
+
+    def forward(self, t: Tensor) -> tuple[Tensor, Tensor]:
+        alpha = torch.ones_like(t)
+        sigma = torch.exp(self.log_sigma_min * (1 - t) + self.log_sigma_max * t)
+        return alpha, sigma
 
 
 class DiffusionProcessor(Processor):
     """Diffusion Processor."""
 
-    def __init__(self, denoiser_nn, loss, schedule: Schedule, **kwargs):
+    def __init__(self, denoiser_nn, loss, schedule: NoiseSchedule, **kwargs):
         """Initialize the DiffusionProcessor.
 
         denoiser_nn: The neural network used for denoising.
@@ -26,3 +52,10 @@ class DiffusionProcessor(Processor):
     def map(self, x: Tensor) -> Tensor:
         """Map input window of states/times to output window using denoiser."""
         return self.denoiser_nn(x)
+    
+    def forward(self, x: Tensor) -> Tensor:
+        return self.map(x)
+
+
+
+
