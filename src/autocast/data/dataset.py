5bc9a7141b98b5599da6794dd52379686b8f27fd
radka-j <r.jersakova@gmail.com>
2025-12-15 10:45:05 +0000

    support normalizing data from file

--- src/autocast/data/dataset.py
+++ src/autocast/data/dataset.py
@@ -3,6 +3,7 @@ from typing import Any, Literal
 
 import h5py
 import torch
+import yaml
 from the_well.data import Augmentation, WellDataset
 from the_well.data.normalization import ZScoreNormalization
 from torch.utils.data import Dataset
@@ -43,7 +44,8 @@ class SpatioTemporalDataset(Dataset, BatchMixin):
         dtype: torch.dtype = torch.float32,
         verbose: bool = False,
         use_normalization: bool = False,
-        norm: type[ZScoreNormalization] | None = None,
+        normalization_type: type[ZScoreNormalization] | None = None,
+        normalization_path: str | None = None,
     ):
         """
         Initialize the dataset.
@@ -75,14 +77,20 @@ class SpatioTemporalDataset(Dataset, BatchMixin):
             If True, print dataset information.
         use_normalization: bool
             Whether to apply Z-score normalization. Defaults to False.
-        norm: type[Standardizer] | None
+        normalization_type: type[Standardizer] | None
             Normalization object (computed from training data). Defaults to None.
+        normalization_path: str | None
+            Path to normalization statistics file (yaml). Defaults to None.
         """
         self.dtype = dtype
         self.verbose = verbose
         self.use_normalization = use_normalization
-        self.norm = norm
+        self.normalization_type = normalization_type
+        self.normalization_path = normalization_path
         self.autoencoder_mode = autoencoder_mode
+        self.metadata: Metadata | None = None
+        # this is the same attribute name that The WellDataset uses
+        self.norm: ZScoreNormalization | None = None
 
         if data_path is not None:
             self.read_data(data_path)
@@ -130,8 +138,9 @@ class SpatioTemporalDataset(Dataset, BatchMixin):
         for traj_idx in range(self.n_trajectories):
             # Create subtrajectories for this trajectory
             fields = (
-                self.data[traj_idx]
-                .unfold(0, self.n_steps_input + self.n_steps_output, self.stride)
+                self.data[traj_idx].unfold(
+                    0, self.n_steps_input + self.n_steps_output, self.stride
+                )
                 # [num_subtrajectories, T_in + T_out, W, H, C]
                 .permute(0, -1, 1, 2, 3)
             )
@@ -243,6 +252,46 @@ class SpatioTemporalDataset(Dataset, BatchMixin):
 
         return self.to_sample(item)
 
+    def set_up_normalization(self):
+        """
+        Set up normalizer.
+        Notes
+        -----
+        - Call method after metadata has been created.
+        - Sets `self.norm` to `None` if `self.use_normalization = False`.
+        """
+
+        # TODO: add handling for when these args are inconsistent with each other
+        if (
+            self.use_normalization
+            and self.normalization_path
+            and self.normalization_type
+        ):
+            if self.metadata is None:
+                msg = "Metadata must be set before normalization."
+                raise ValueError(msg)
+            with open(self.normalization_path, mode="r") as f:
+                stats = yaml.safe_load(f)
+            if len(self.metadata.field_names.keys()) > 1:
+                # TODO: in The Well they separately track:
+                #   - `core_field_names` (vs field_names) and
+                #   - `core_constant_field_names` (vs constant_field_names)
+                # e.g., core is [`velocity`] instead of [`velocity_x`, `velocity_y`]
+                # we need a way to do this (once we have datasets that require it)
+                msg = (
+                    "Staggered grid normalization not implemented. Found "
+                    f"field_names indices {list(self.metadata.field_names.keys())}, but "
+                    "only index 0 is currently supported."
+                )
+                raise NotImplementedError(msg)
+            self.norm = self.normalization_type(
+                stats,
+                self.metadata.field_names[0],
+                [],
+            )
+        else:
+            self.norm = None
+
 
 class ReactionDiffusionDataset(SpatioTemporalDataset):
     """Reaction-Diffusion dataset."""
@@ -292,6 +341,7 @@ class AdvectionDiffusionDataset(SpatioTemporalDataset):
             n_steps_per_trajectory=[self.data.shape[1]] * self.data.shape[0],
             grid_type="cartesian",
         )
+        self.set_up_normalization()
 
 
 class BOUTDataset(SpatioTemporalDataset):
@@ -320,6 +370,7 @@ class BOUTDataset(SpatioTemporalDataset):
             n_steps_per_trajectory=[self.data.shape[1]] * self.data.shape[0],
             grid_type="cartesian",
         )
+        self.set_up_normalization()
 
 
 class TheWell(SpatioTemporalDataset):
