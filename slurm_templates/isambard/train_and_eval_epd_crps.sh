cebba5312978a7e31b400ca6e25487ae90ed7baf
Sam Greenbury <sgreenbury@turing.ac.uk>
2026-02-10 14:32:42 +0000

    Refactor and update CRPS script
    Co-authored-by: Radka Jersakova <r.jersakova@gmail.com>
    Co-authored-by: ContiPaolo <ContiPaolo@users.noreply.github.com>

--- slurm_templates/isambard/train_and_eval_epd_crps.sh
+++ slurm_templates/isambard/train_and_eval_epd_crps.sh
@@ -5,43 +5,27 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=0
 #SBATCH --job-name epd_and_eval
-#SBATCH --output=logs/epd_and_eval%j.out
-#SBATCH --error=logs/epd_and_eval%j.err
+#SBATCH --output=logs/epd_and_eval_%j.out
+#SBATCH --error=logs/epd_and_eval_%j.err
 
 set -e
 
+# Might be used within python scripts
 export AUTOCAST_DATASETS="$PWD/datasets"
 
 # Set configuration parameters
-DATAPATH="advection_diffusion_multichannel_64_64"
-USE_NORMALIZATION="false"
-# MODEL="fno" # ViT
-MODEL="vit_large" # FNO
-HIDDEN_DIM=256
-MODEL_NOISE="concat" # Options: "cln", "concat"
+DATAPATH="advection_diffusion_multichannel_64_64" # Options: "advection_diffusion_multichannel_64_64", "advection_diffusion_multichannel"
+USE_NORMALIZATION="false" # Options: "true" or "false"
+MODEL="vit_large" # Options (any compatible config in configs/processors/), currently: "vit", "vit_large", "fno"
+HIDDEN_DIM=256 # Any positive integer, e.g. 256, 512, 1024, etc.
+MODEL_NOISE="concat" # Options: "cln", "concat", "additive"
 
-# Derive remaining parameters based on the dataset and model choices
+# These assume a single noise per spatial point (not per time step).
 if [ ${DATAPATH} == "advection_diffusion_multichannel_64_64" ]; then
     NOISE_CHANNELS=4096
-else
+elif [ ${DATAPATH} == "advection_diffusion_multichannel" ]; then
     NOISE_CHANNELS=1024
 fi
-GIT_HASH=$(git rev-parse --short=7 HEAD | tr -d '\n')
-UUID=$(uuidgen | tr -d '\n' | tail -c 7)
-RUN_NAME="${DATAPATH}_${MODEL}_${MODEL_NOISE}_${HIDDEN_DIM}_crps_no_norm_${GIT_HASH}_${UUID}"
-WORKING_DIR="$PWD/outputs/$(date +%F)/${RUN_NAME}/"
-
-mkdir -p $WORKING_DIR
-exec > "${WORKING_DIR}/slurm_${SLURM_JOB_NAME}_${SLURM_JOB_ID}.out" \
-     2> "${WORKING_DIR}/slurm_${SLURM_JOB_NAME}_${SLURM_JOB_ID}.err"
-
-
-# Flow matching
-# MODEL_PARAMS=(
-#     "optimizer.learning_rate=0.0002"
-#     "encoder@model.encoder=dc_deep_256"
-#     "decoder@model.decoder=dc_deep_256"
-# )
 
 # Hidden dimension parameters
 if [ ${MODEL} == "vit_large" ]; then
@@ -97,12 +81,27 @@ MODEL_PARAMS+=(
      "+model.train_metrics.crps._target_=autocast.metrics.ensemble.CRPS"
 )
 
+# Derive code and unique run identifiers
+GIT_HASH=$(git rev-parse --short=7 HEAD | tr -d '\n')
+UUID=$(uuidgen | tr -d '\n' | tail -c 7)
+
+# Run name and working directory
+RUN_NAME="crps_${DATAPATH}_${MODEL}_${MODEL_NOISE}_${HIDDEN_DIM}_${GIT_HASH}_${UUID}"
+WORKING_DIR="$PWD/outputs/$(date +%F)/${RUN_NAME}/"
+
+# Check if there's a pretrained autoencoder checkpoint in the working directory
 CKPT="${WORKING_DIR}/autoencoder.ckpt"
 if [ -f "${CKPT}" ]; then
         MODEL_PARAMS+=( "+autoencoder_checkpoint=${CKPT}" )
 fi
 
-# Training (many hrs)
+# Make directories and redirect output and error logs to the working directory
+mkdir -p $WORKING_DIR
+exec > "${WORKING_DIR}/slurm_${SLURM_JOB_NAME}_${SLURM_JOB_ID}.out" \
+     2> "${WORKING_DIR}/slurm_${SLURM_JOB_NAME}_${SLURM_JOB_ID}.err"
+
+
+# Training
 srun uv run train_encoder_processor_decoder \
     hydra.run.dir=${WORKING_DIR} \
 	datamodule="${DATAPATH}" \
@@ -112,9 +111,8 @@ srun uv run train_encoder_processor_decoder \
 	logging.wandb.name="${RUN_NAME}" \
 	 "${MODEL_PARAMS[@]}"
 	
-	
-	
-# Eval (5-10 mins)
+
+# Eval
 CKPT_PATH="${WORKING_DIR}/encoder_processor_decoder.ckpt"
 EVAL_DIR="${WORKING_DIR}/eval"
 
